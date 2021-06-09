007f073d3fcdfee761b22b5b29fe8ac7d078d95b
Derek Hudgins <hudgins.derek@gmail.com>
2021-06-08 17:50:40 -0700

    added new css

--- src/ImageList.js
+++ src/ImageList.js
@@ -7,19 +7,19 @@ export default class ImageList extends Component {
         hornFilter: ''
     }
 
-    handleChange1 = e => {
+    Filter1 = e => {
         this.setState({
             keyFilter: e.target.value
         });
     }
 
-    handleChange2 = e => {
+    Filter2 = e => {
         this.setState({
             hornFilter: e.target.value
         });
     }
 
-    
+
     render() {
         const keyAnimals = this.props.images.filter((animal) => {
             if (!this.state.keyFilter) return true;
@@ -32,8 +32,29 @@ export default class ImageList extends Component {
             return false;
         });
         return (
-            
             <div>
+                <span className='filter-text'>Filter animals by keyword or number of horns: </span>
+                <select className='dropdown' onChange={this.Filter1}>
+                    <option value=''>Show All</option>
+                    <option value='rhino'>Rhino</option>
+                    <option value='narwhal'>Narwhal</option>
+                    <option value='unicorn'>Unicorn</option>
+                    <option value='unilego'>Unilego</option>
+                    <option value='triceratops'>Triceratops</option>
+                    <option value='markhor'>Markhor</option>
+                    <option value='mouflon'>Mouflon</option>
+                    <option value='addax'>Addax</option>
+                    <option value='chameleon'>Chameleon</option>
+                    <option value='lizard'>Lizard</option>
+                    <option value='dragon'>Dragon</option>
+                </select>
+                <select onChange={this.Filter2} >
+                    <option value=''>Show All</option>
+                    <option value='1'>Just One</option>
+                    <option value='2'>Two</option>
+                    <option value='3'>Three</option>
+                    <option value='100'>100</option>
+                </select>
                 <div className='image-list'>
                     {
                     FilteredAnimals.map(animal =>
